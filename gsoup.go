963b7138382b7518fdee9874d1e3aa48871459ad
Oyewale Ademola <saopayne@gmail.com>
2017-09-23 05:51:44 +0100

    Add DOM parsing to fetch next sibling, previous sibling and attributes of a node

--- gsoup.go
+++ gsoup.go
@@ -177,7 +177,7 @@ func GetKeyValue(attributes []html.Attribute) map[string]string {
 }
 
 // Catch panics when they occur
-func CatchPanic(fnName string) {
+func localPanic(fnName string) {
 	if r := recover(); r != nil {
 		log.Println("Error occurred in", fnName, ":", r)
 	}
@@ -192,14 +192,18 @@ type Root struct {
 
 var debug = false
 
-// SetDebug sets the debug status
-func SetDebug(d bool) {
-	debug = d
+func EnableDebug() {
+	debug = true
+}
+
+func DisableDebug() {
+	debug = false
 }
 
 // Get returns the HTML returned by the url in string
+// Accepts the url and gets the content of the url page
 func Get(url string) (string, error) {
-	defer CatchPanic("Get()")
+	defer localPanic("Get()")
 	resp, err := http.Get(url)
 	if err != nil {
 		if debug {
@@ -222,13 +226,15 @@ func Get(url string) (string, error) {
 	return string(bytes), nil
 }
 
+func HtmlToPlainText(s string){}
+
 // HTMLParse parses the HTML returning a start pointer to the DOM
 func HTMLParse(s string) Root {
-	defer CatchPanic("HTMLParse()")
+	defer localPanic("HTMLParse()")
 	r, err := html.Parse(strings.NewReader(s))
 	if err != nil {
 		if debug {
-			panic("Unable to parse the HTML")
+			panic("Unable to parse the provided HTML")
 		}
 
 		return Root{nil, "", errors.New("Unable to parse the HTML")}
@@ -244,7 +250,6 @@ func HTMLParse(s string) Root {
 		case html.CommentNode:
 			r = r.NextSibling
 		}
-
 	}
 	return Root{r, r.Data, nil}
 }
@@ -253,7 +258,7 @@ func HTMLParse(s string) Root {
 // with or without attribute key and value specified,
 // and returns a struct with a pointer to it
 func (r Root) Find(args ...string) Root {
-	defer CatchPanic("Find()")
+	defer localPanic("Find()")
 	temp, ok := FindOnce(r.Pointer, args, false)
 	if ok == false {
 		if debug {
@@ -271,7 +276,7 @@ func (r Root) Find(args ...string) Root {
 // and returns an array of structs, each having
 // the respective pointers
 func (r Root) FindAll(args ...string) []Root {
-	defer CatchPanic("FindAll()")
+	defer localPanic("FindAll()")
 	temp := FindAllofem(r.Pointer, args)
 	if len(temp) == 0 {
 		if debug {
@@ -292,7 +297,7 @@ func (r Root) FindAll(args ...string) []Root {
 // FindNextSibling finds the next sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindNextSibling() Root {
-	defer CatchPanic("FindNextSibling()")
+	defer localPanic("FindNextSibling()")
 	nextSibling := r.Pointer.NextSibling
 	if nextSibling == nil {
 		if debug {
@@ -308,7 +313,7 @@ func (r Root) FindNextSibling() Root {
 // FindPrevSibling finds the previous sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindPrevSibling() Root {
-	defer CatchPanic("FindPrevSibling()")
+	defer localPanic("FindPrevSibling()")
 	prevSibling := r.Pointer.PrevSibling
 	if prevSibling == nil {
 		if debug {
@@ -324,7 +329,7 @@ func (r Root) FindPrevSibling() Root {
 // FindNextElementSibling finds the next element sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindNextElementSibling() Root {
-	defer CatchPanic("FindNextElementSibling()")
+	defer localPanic("FindNextElementSibling()")
 	nextSibling := r.Pointer.NextSibling
 	if nextSibling == nil {
 		if debug {
@@ -345,7 +350,7 @@ func (r Root) FindNextElementSibling() Root {
 // FindPrevElementSibling finds the previous element sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindPrevElementSibling() Root {
-	defer CatchPanic("FindPrevElementSibling()")
+	defer localPanic("FindPrevElementSibling()")
 	prevSibling := r.Pointer.PrevSibling
 	if prevSibling == nil {
 		if debug {
@@ -365,7 +370,7 @@ func (r Root) FindPrevElementSibling() Root {
 
 // Attrs returns a map containing all attributes
 func (r Root) Attrs() map[string]string {
-	defer CatchPanic("Attrs()")
+	defer localPanic("Attrs()")
 	if r.Pointer.Type != html.ElementNode {
 		if debug {
 			panic("Not an ElementNode")
@@ -383,7 +388,7 @@ func (r Root) Attrs() map[string]string {
 
 // Text returns the string inside a non-nested element
 func (r Root) Text() string {
-	defer CatchPanic("Text()")
+	defer localPanic("Text()")
 	k := r.Pointer.FirstChild
 checkNode:
 	if k.Type != html.TextNode {
